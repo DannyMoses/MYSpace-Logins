4f37525926ebf45dbe065c50f8379556768837de
Ubuntu <ubuntu@myspace-logins-1.c.winged-sol-258623.internal>
2019-11-17 22:21:08 +0000

    Changed config files to connect to remote SQL databases

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 CONFIG= {
-	"profiles_ip" : "PROFILES_SERVICE_IP"
+	"profiles_ip" : "PROFILES_SERVICE_IP",
+
+	"mysql_usr" : "[user]",
+	"mysql_pwd" : "[password]",
+	"mysql_server" : "[db ip]",
+	"mysql_port" : "[db service port]",
+	"mysql_db" : "[db name]"
 }
