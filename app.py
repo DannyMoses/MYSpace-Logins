e15d0a48818d8d7bc0f140b1ee70e497dd1a20ab
Ubuntu <ubuntu@myspace-logins-1.c.winged-sol-258623.internal>
2019-11-20 09:37:26 +0000

    Fixed check_user(). Added logging

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, request, session, render_template
 #from flask_pymongo import PyMongo
 #from flask_sqlalchemy import SQLAlchemy
 from itsdangerous import URLSafeTimedSerializer
-import bcrypt
-import requests
+import bcrypt, requests
 
-import json, re, smtplib, ssl
+import logging, json, re, smtplib, ssl
 from datetime import date
 from config import CONFIG
 
@@ -47,6 +46,12 @@ gmail.get_credentials()
 
 EMAIL_REGEX = re.compile(r"([A-Z0-9_.+-]+@[A-Z0-9-]+.[A-Z0-9-.]+)", re.IGNORECASE)
 
+# Setup logging
+if __name__ != '__main__':
+	gunicorn_logger = logging.getLogger('gunicorn.error')
+	app.logger.handlers = gunicorn_logger.handlers
+	app.logger.setLevel(gunicorn_logger.level)
+
 def gen_token(email):
 	serializer = URLSafeTimedSerializer(app.config["SECRET_KEY"])
 	return serializer.dumps(email, salt=app.config["SECURITY_PASSWORD_SALT"])
@@ -65,51 +70,63 @@ def confirm_token(token, expiration=10000):
 	return email
 
 def check_user(user):
-	sql = "SELECT username FROM users WHERE username = '" + user + "'" 
+	sql = "SELECT username FROM users WHERE username=%s" 
+	#app.logger.info("check_user SQL: " + sql)
 	print("SQL:", sql)
-	x = cursor.execute(sql)
-	print(x)
-	if x is None:
-		return False
-	else:
-		return True
+	cursor.execute(sql, (user,))
+	app.logger.debug("Result: ")
+	for x in cursor:
+		app.logger.debug(x)
+		if user in x:
+			return True
 	return False
 
 def check_email(email):
 	sql = "SELECT email FROM users WHERE email=%s"
+	#app.logger.info("check_email SQL: " + sql)
 	x = cursor.execute(sql, (email,))
+	app.logger.debug("Result: ")
 	for x in cursor:
+		app.logger.debug(x)
 		if email in x:
 			return True
 	return False
 
 def check_validated(user):
 	sql = "SELECT validated FROM users WHERE username = %s"
+	#app.logger.info("check_validated SQL: " + sql)
 	cursor.execute(sql, (user,))
 	for x in cursor:
+		app.logger.debug(x)
 		return x[0] == 1
 	return False
 
 def get_pwhash(user):
 	sql = "SELECT password_hash FROM users where username = %s"
+	#app.logger.info("check_pwhash SQL: " + sql)
 	cursor.execute(sql, (user,))
 	for x in cursor:
+		app.logger.debug(x)
 		return x[0]
 	return None
 
+# Reset
 @app.route('/reset_logins', methods=["POST"])
 def reset():
 	print("/RESET_LOGINS() CALLED")
+	app.logger.warning("/RESET_LOGINS() CALLED")
 	cursor.execute("DELETE FROM users")
 	db.commit()
 	return { "status": "OK" }, 200
 
+# Adduser
 @app.route('/adduser', methods=["POST"])
 def add_user():
 	print(80*'=')
 	print("/ADDUSER() CALLED")
 	data = request.json
 	print("DATA: ", str(data))
+	app.logger.debug("/adduser data: " + json.dumps(data))
 
 	if data is None:
 		print("error: DATA IS NONE")
@@ -120,8 +137,6 @@ def add_user():
 		return { "status" : "error", "error": "Not all data fields were found" }, 400
 
 	uname = data["username"]
-	# usr_collection = mongo.db.users
-	# usr_collection is not None and usr_collection.find_one({"username" : uname }) is not None
 
 	email = data["email"]
 	res = EMAIL_REGEX.match(email)
@@ -129,10 +144,17 @@ def add_user():
 	if res == None:
 		print("error: BAD EMAIL REGEX MATCH")
 		return { "status" : "error" }, 400
-	if check_email(email) is True:
+	if check_email(email):
 		print("error: DATABASE CHECK EMAIL FAILED")
 		return { "status" : "error", "error": "Email already in use" }, 400
 
+	app.logger.debug("/adduser email {} check passed".format(data['email']))
+
+	if check_user(uname):
+		print("error: DATABASE CHECK USERNAME FAILED")
+		return { "status" : "error", "error": "Username taken" }, 400
+
+	app.logger.debug("/adduser user {} check passed".format(data['username']))
 
 	hashed = bcrypt.hashpw(data["password"].encode('utf8'), bcrypt.gensalt())
 	hashed = hashed.decode('utf-8')
@@ -144,15 +166,10 @@ def add_user():
 		"validated" : False
 		}
 
-	#usr_collection.insert_one(usr)
 	sql = "INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s)"
 	print(type(hashed))
 	val = (uname, email, hashed)
 	cursor.execute(sql, val)
-
-	if check_user(uname) == True:
-		print("error: DATABASE CHECK USERNAME FAILED")
-		return { "status" : "error", "error": "Username taken" }, 400
 
 	db.commit()
 
@@ -175,12 +192,13 @@ def add_user():
 
 	return { "status" : "OK" }, 200
 
-
+# Verify
 @app.route("/verify", methods=["POST"])
 def verify_user():
 	print(80*'=')
 	print("/VERIFY() CALLED")
 	data = request.json
+	app.logger.debug("/verify data: " + json.dumps(data))
 
 	if "email" not in data or "key" not in data:
 		return { "status" : "error", "error": "Email or key not specified in request" }, 200 #400
@@ -207,19 +225,15 @@ def verify_user():
 	db.commit()
 	return { "status" : "OK"}, 200
 
+# Login
 @app.route('/login', methods=["POST"])
 def login():
 	print(80*'=')
 	print("/LOGIN() CALLED")
 	creds = request.json
 	print("CREDS:", creds)
+	app.logger.debug("/login creds: " + json.dumps(creds))
 	#users_c = mongo.db.users
-
-	#user = users_c.find_one({'username': creds['username']})
-
-	# Already logged in
-	#if session['username']:
-	#	return { "status" : "OK"}, 200
 
 	# User does not exist
 	if check_user(creds['username']) == False: 
@@ -227,20 +241,24 @@ def login():
 		return { "status" : "error", "error" : "Username not found" }, 400
 
 	# print("USER VALID", str(user['validated']))
+	app.logger.debug("/login user {} exists".format(creds['username']))
 
-	if not check_validated(creds['username']):
+	if check_validated(creds['username']) == False:
 		print("NOT VALIDATED")
 		return { "status" : "error", "error" : "User has not been validated" }, 400
-	
+
+	app.logger.debug("/login user {} validated".format(creds['username']))
 	
 	if bcrypt.checkpw(creds['password'].encode('utf8'), get_pwhash(creds['username']).encode('utf8')):
-		# session['username'] = creds['username']
 		print("LOGIN GOOD")
 	else:
 		return { "status" : "error", "error" : "Incorrect password" }, 400
 
+	app.logger.debug("/login user {} login good".format(creds['username']))
+
 	return { "status" : "OK", "username": creds['username'] }, 200
 
+# Logout
 @app.route('/logout', methods=["POST"])
 def logout():
 	session.clear()
